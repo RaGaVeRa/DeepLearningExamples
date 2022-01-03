b2ec5486c1abdf74abbabe43f3ee0d2c33b2f23e
Shiva Kumar H R <shivahr@gmail.com>
2022-01-03 19:42:30 +0530

    Script for running inference on kn_f_sg

---
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/infer_base.sh
@@ -0,0 +1,10 @@
+OUT_DIR=./output/${DATASET_NAME}/${DATASET_NAME}-infer-tacotron2-waveglow/tacotron2_${TACOTRON2_CHECKPOINT_NUMBER}-waveglow_${WAVEGLOW_CHECKPOINT_NUMBER}
+TACOTRON2_CHECKPOINT=./output/${DATASET_NAME}/tacotron2_transferlearn/checkpoint_Tacotron2_${TACOTRON2_CHECKPOINT_NUMBER}.pt
+WAVEGLOW_CHECKPOINT=./output/${DATASET_NAME}/waveglow_transferlearn/checkpoint_WaveGlow_${WAVEGLOW_CHECKPOINT_NUMBER}.pt
+INPUT_PHRASES=infer/${DATASET_NAME}_phrases.txt
+mkdir -p $OUT_DIR
+cp ${INPUT_PHRASES} $OUT_DIR/
+CMD="python inference.py --tacotron2 ${TACOTRON2_CHECKPOINT} --waveglow ${WAVEGLOW_CHECKPOINT} -d 0.025 --wn-channels 256 -o ${OUT_DIR} -i ${INPUT_PHRASES} --fp16"
+echo $CMD > $OUT_DIR/infer.sh
+$CMD
+cd $OUT_DIR; git add *; git commit -m"Tacotron2 & Waveglow Inference"; git push
